4bd4b55398fc9b89ea7555f21aa70d3392b450ea
dmwilliams <danielmarkwilliams@gmail.com>
2023-06-26 21:44:37 -0500

    Update index.html and script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,11 @@ var $search = $('#search');
 var $input = $('#input');
 // html element checkboxes /// need to add the the element ids
 var $hotelCheck = $('#hotelCheck');
-var $restrauntCheck = $('#restrauntCheck');
+var $restaurantCheck = $('#restaurantCheck');
 var $attractionCheck = $('#attractionCheck');
 // checkbox variables to be used in js
 var hotelCheck;
-var restrauntCheck;
+var restaurantCheck;
 var attractionCheck;
 // loaction varible used in js
 var city;
@@ -24,12 +24,12 @@ $( function ()
         $input.attr('placeholder','enter city');
         // get values from checkboxes
         hotelCheck = $hotelCheck.is(":checked");
-        restrauntCheck = $restrauntCheck.is(":checked");
+        restaurantCheck = $restaurantCheck.is(":checked");
         attractionCheck = $attractionCheck.is(":checked");
         //check inputs in console
         console.log("user input: " + city);
         console.log("hotel check: " + hotelCheck);
-        console.log("restraunt check: " + restrauntCheck);
+        console.log("restaurant check: " + restaurantCheck);
         console.log(" attractions check: "+ attractionCheck);
     });
 
