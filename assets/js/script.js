8e52fd3274f1a2bf627e4e2bf54f36de305466e4
Camille MacLaren <camillemaclaren@gmail.com>
2023-06-26 20:15:31 -0500

    more comments and start of checkboxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,28 @@
+// init variables
 var $search = $('#search');
+// html element checkboxes /// need to add the the element ids
 var $hotelCheck = $();
 var $restrauntCheck = $();
 var $attractionCheck = $();
+// checkbox variables to be used in js
+var hotelCheck;
+var restrauntCheck;
+var attractionCheck;
 
 $( function () 
 {
     $search.submit( function(event)
     {
         event.preventDefault();
-        //get city name
+        // get city name
         city = $input.val();
-        city = city.replace('+', ' ');
-        //clear text area and give it a placeholder
+        // clear text area and give it a placeholder
         $input.val('');
         $input.attr('placeholder','enter city');
+        // get values from checkboxes
+        hotelCheck = $hotelCheck.is(":checked");
+        restrauntCheck = $restrauntCheck.is(":checked");
+        attractionCheck = $attractionCheck.is(":checked");
     });
 
 });
