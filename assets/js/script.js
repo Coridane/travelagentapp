b16789beb9a81eea7077c5ef46dfd151c442e231
Camille MacLaren <camillemaclaren@gmail.com>
2023-06-28 19:32:52 -0500

    Toggable cards
    the cards will be displayed depending if the user checked their respective checkboxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,10 @@ var $input = $('#input');
 var $hotelCheck = $('#hotelCheck');
 var $restaurantCheck = $('#restaurantCheck');
 var $attractionCheck = $('#attractionCheck');
+//card elements
+var $hotel = $('#hotel');
+var $restaurant = $('#restaurant');
+var $attraction = $('#attraction');
 // checkbox variables to be used in js
 var hotelCheck;
 var restaurantCheck;
@@ -31,9 +35,49 @@ $( function ()
         console.log("hotel check: " + hotelCheck);
         console.log("restaurant check: " + restaurantCheck);
         console.log(" attractions check: "+ attractionCheck);
+        //call display cards
+        toggleCards();
     });
 
 });
+
+
+function toggleCards()
+{
+    //hotel check
+    if(hotelCheck === true) //show card if checked
+    {
+        $hotel.attr('class', 'display');
+        $hotel.attr('style','display: block;');
+    }
+    else
+    {
+        $hotel.attr('class', 'hidden');
+        $hotel.attr('style','display: none;');
+    }
+    //restaurant
+    if(restaurantCheck === true) //show card if checked
+    {
+        $restaurant.attr('class', 'display');
+        $restaurant.attr('style','display: block;');
+    }
+    else
+    {
+        $restaurant.attr('class', 'hidden');
+        $restaurant.attr('style','display: none;');
+    }
+    //attraction check
+    if(attractionCheck === true) //show card if checked
+    {
+        $attraction.attr('class', 'display');
+        $attraction.attr('style','display: block;');
+    }
+    else
+    {
+        $attraction.attr('class', 'hidden');
+        $attraction.attr('style','display: none;');
+    }
+}
 
 // Loop for team logo video
 document.getElementById('video').addEventListener('ended',myHandler,false);
