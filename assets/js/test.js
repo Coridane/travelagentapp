1a9ad3d66460eb223349d20033418e1078e4b923
abstractl0gic <zwlyons@gmail.com>
2023-06-29 20:01:43 -0500

    add loop to limit search results

--- assets/js/test.js
+++ assets/js/test.js
@@ -58,6 +58,21 @@ autocomplete.addListener('place_changed', function() {
 });
 
 
+// Autocomplete for user search input
+var autocomplete = new google.maps.places.Autocomplete($input[0]);
+
+// use an eventlistener for 'input event
+autocomplete.addListener('place_changed', function() {
+  var place = autocomplete.getPlace();
+  if (place && place.formatted_address) {
+    city = place.formatted_address;
+  }
+});
+
+
+function initMap() {
+    // Your map initialization code goes here
+  }
 function toggleCards() {
   //displays card depending on checkbox value
   $hotel.toggle(hotelCheck);
