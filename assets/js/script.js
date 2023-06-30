e5d93e1791b061793413c64d333a88d6eb396d15
Camille MacLaren <camillemaclaren@gmail.com>
2023-06-30 02:24:18 -0500

    update save function
    indent for readability
    put save function outside of eventlistener
    added a repeat checker
    prevent undefined items from being added
    add start of search again when item in sidebar is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // init variables
 var $search = $('#search');
 var $input = $('#input');
+var $sidebar = $('#mySidebar');
 // html element checkboxes /// need to add the element ids
 var $hotelCheck = $('#hotelCheck');
 var $restaurantCheck = $('#restaurantCheck');
@@ -27,81 +28,105 @@ var saveCity = function(cty)
 $(function () {
     //displays the save list in the side bar
     displaySaveList();
-  // Autocomplete for user search input
-  var autocompleteService = new google.maps.places.AutocompleteService();
-  var autocomplete = new google.maps.places.Autocomplete($input[0]);
+    // Autocomplete for user search input
+    var autocompleteService = new google.maps.places.AutocompleteService();
+    var autocomplete = new google.maps.places.Autocomplete($input[0]);
 
-  // Listen for 'place_changed' event
-  autocomplete.addListener('place_changed', function() {
-    var place = autocomplete.getPlace();
-    if (place && place.formatted_address) {
-      city = place.formatted_address;
-    }
-  });
+    // Listen for 'place_changed' event
+    autocomplete.addListener('place_changed', function() {
+        var place = autocomplete.getPlace();
+        if (place && place.formatted_address) {
+        city = place.formatted_address;
+        }
+    });
 
-  $search.submit(function (event) {
-    event.preventDefault();
-    // clear text area and give it a placeholder
-    $input.val('');
-    $input.attr('placeholder', 'Enter a city');
-    // get values from checkboxes
-    hotelCheck = $hotelCheck.is(":checked");
-    restaurantCheck = $restaurantCheck.is(":checked");
-    attractionCheck = $attractionCheck.is(":checked");
-    // check inputs in console
-    console.log("user input: " + city);
-    console.log("hotel check: " + hotelCheck);
-    console.log("restaurant check: " + restaurantCheck);
-    console.log("attractions check: " + attractionCheck);
-    // call display cards
-    toggleCards();
-    // checks to see if at least one checkbox is checked 
-    if (hotelCheck == false && restaurantCheck == false && attractionCheck == false) {
-      $('#alert').show();
-    } else {
-      $('#alert').hide();
-    }
-  });
+    $search.submit(function (event) {
+        event.preventDefault();
+        // clear text area and give it a placeholder
+        $input.val('');
+        $input.attr('placeholder', 'Enter a city');
+        // get values from checkboxes
+        hotelCheck = $hotelCheck.is(":checked");
+        restaurantCheck = $restaurantCheck.is(":checked");
+        attractionCheck = $attractionCheck.is(":checked");
+        // check inputs in console
+        console.log("user input: " + city);
+        console.log("hotel check: " + hotelCheck);
+        console.log("restaurant check: " + restaurantCheck);
+        console.log("attractions check: " + attractionCheck);
+        // call display cards
+        toggleCards();
+        // checks to see if at least one checkbox is checked 
+        if (hotelCheck == false && restaurantCheck == false && attractionCheck == false) {
+            $('#alert').show();
+        } else {
+            $('#alert').hide();
+        }
+     });
 
     //store save on button click
-    $save.on("click",function(event)
-    {
-        //init array
-        var saveList = [];
-        console.log("city in save button " + city);
-        //checks if the local storage has stuff and if it does the save List is the storage
-        if (localStorage.getItem('saveList') !== null) 
-        {
-            saveList = JSON.parse(localStorage.getItem("saveList"));
-        }
-        if(city !== '')
-        {
-            //create new JSON object
-            var newCity = new saveCity(city);
-            //add object to array
-            saveList.push(newCity);
-            console.log("saving this city: " + saveList);
-            //set the storage with the updated array
-            localStorage.setItem("saveList",JSON.stringify(saveList)); 
-        }
-        displaySaveList();
-        
-    })
+    $save.on("click", save);
+    $sidebar.on("click",'.w3-bar-item', searchAgain);
 
 });
 
-function toggleCards() 
+function searchAgain()
 {
-  // displays card depending on checkbox value
-  $hotel.toggle(hotelCheck);
-  $restaurant.toggle(restaurantCheck);
-  $attraction.toggle(attractionCheck);
+    city =  $(this).text();
+    console.log("city pressed: " + city);
+}
 
+
+function save()
+{
+    console.log("city to be save: " + city);
+    if(city == undefined)
+        return;
+    //init array
+    var saveList = [];
+    //console.log("city in save button " + city);
+    //checks if the local storage has stuff and if it does the save List is the storage
+    if (localStorage.getItem('saveList') !== null) 
+    {
+        saveList = JSON.parse(localStorage.getItem("saveList"));
+    }
+    if(city !== '' )
+    {
+        //create new JSON object
+        var newCity = new saveCity(city);
+        //check if city already exists
+        var replace = checkRepeat(saveList, city);
+        //if there is a repeat remove it from the list from its previous postion
+        if( replace !== false)
+        {
+            saveList.splice(replace, 1);
+        }  
+        //add object to array
+        saveList.push(newCity);
+        //set the storage with the updated array
+        localStorage.setItem("saveList",JSON.stringify(saveList)); 
+    }
+    displaySaveList();
+
+}
+
+function checkRepeat(saveList, city)
+{
+    //checks if there is a repeat and send the index at repeat
+    var ind;
+    for(var i = 0; i < saveList.length; i++)
+    {
+        if(saveList[i].city == city)
+        {
+            ind = i;
+            return ind;
+        }         
+    }
+    return false;
 }
 
 function displaySaveList()
 {
-    var $sidebar = $('#mySidebar');
     var $btn = $('<button onclick="w3_close()" class="w3-bar-item w3-large">Close &times;</button>');
     //remove everything in side bar
     $sidebar.children().remove();
@@ -118,7 +143,6 @@ function displaySaveList()
         for(i = saveList.length; i-- ; i > 0)
         {
             var city = saveList[i].city;
-            console.log("city in side bar: "+ city);
             var $a= $('<a href="#" class="w3-bar-item w3-button">' + city + '</a>');
             //add to list
             $sidebar.append($a);
