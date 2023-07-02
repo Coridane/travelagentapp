4aee18272363917efc586f90095f5c41742da862
Camille MacLaren <camillemaclaren@gmail.com>
2023-07-01 23:58:55 -0500

    comments for readability
    renamed some elements and add another class to card items

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,13 +46,10 @@ $(function () {
         // clear text area and give it a placeholder
         $input.val('');
         $input.attr('placeholder', 'Enter a city');
-        
         // check inputs in console
         console.log("user input: " + city);
-       
         // call display cards
         toggleCards();
-        
         //displays the api calls on html
         fetchResults();
      });
@@ -68,9 +65,11 @@ function searchAgain()
     //gets the city that was pressed
     city =  $(this).text();
     console.log("city pressed: " + city);
+    //displays depending on checkboxes
     toggleCards();
+    //shows the results from API fetch
     fetchResults();
-    //save again
+    //save again (this will make item at top of save list)
     save();
 }
 
@@ -87,8 +86,7 @@ function save()
     {
         saveList = JSON.parse(localStorage.getItem("saveList"));
     }
-    
-    //create new JSON object
+    //create new JSON city object
     var newCity = new saveCity(city);
     //check if city already exists
     var replace = checkRepeat(saveList, city);
@@ -125,7 +123,7 @@ function displaySaveList()
 {
     //this function updates the sidebar html
     var $btn = $('<button onclick="w3_close()" class="w3-bar-item w3-large">Close &times;</button>');
-    //remove everything in side bar
+    //remove everything in side bar to avoid repeats
     $sidebar.children().remove();
     //add the close the button
     $sidebar.append($btn);
@@ -147,8 +145,6 @@ function displaySaveList()
     }
 }
 
-
-
 function toggleCards()
 {
     // get values from checkboxes
@@ -163,8 +159,7 @@ function toggleCards()
     console.log("hotel check: " + hotelCheck);
     console.log("restaurant check: " + restaurantCheck);
     console.log("attractions check: " + attractionCheck);
-    //warning
-    // checks to see if at least one checkbox is checked 
+    //warning for when there are no checkboxes checked
     if (hotelCheck == false && restaurantCheck == false && attractionCheck == false) {
         $('#alert').show();
     } else {
@@ -191,12 +186,12 @@ function w3_close() {
 }
 //end of sidebar
 
-//API stuff
+//API stuff-----------------------------------------------
 
 function fetchResults()
 {
   console.log("city in fetch Results " + city);
-  // Fetch hotels using Google Places API
+  //fetch info depeding on checkboxes
   if (hotelCheck) {
     fetch(city,'hotel');
   }
@@ -208,19 +203,24 @@ function fetchResults()
   }
 }
 
-// Fetch hotels using Google Places API
+// Fetch depending on type using Google Places API
 function fetch(city, type) {
   var request = {
     query: type +'s in ' + city,
     fields: ['name', 'formatted_address', 'rating', 'photos'],
   };
-  console.log('type: ' + type);
+  //reference the type card in html
+  var $element = $('#'+type);
+
   var service = new google.maps.places.PlacesService(document.createElement('div'));
-  console.log(service);
+  //look for the results
   service.textSearch(request, function (results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
-      $('#'+type).empty(); // Clear existing hotels
-
+      // Clear existing info
+      $element.empty(); 
+      //re-add img header
+      var img = $('<img src="./assets/img/'+type+'s.png">');
+      $element.append(img);
       // Loop through the results and create hotel cards
       var userSearch = 0; // this is to keep track of the number of results.
       results.forEach(function (result) {
@@ -236,9 +236,10 @@ function fetch(city, type) {
   });
 }
 
-// Function to create a attraction card element
+// Function to create a card element
 function createCard(results, type) {
   var card = $('<div>').addClass(type+'-card');
+  card.addClass('mini-card');
   var name = $('<h3>').text(results.name);
   var address = $('<p>').text(results.formatted_address);
   var rating = $('<p>').text('Rating: ' + results.rating);
